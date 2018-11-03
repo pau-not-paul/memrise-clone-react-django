0a7995202e938c0d1b79fffc1e616c54f52f8065
Pau Vidal <quinvols@gmail.com>
2018-11-03 11:00:34 +0100

    Articles removed.

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('articles.api.urls')),
     path('courses/', include('courses.api.urls')),
 ]
